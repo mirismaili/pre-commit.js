6f41915bfcfd8ddf6622226a3c0c094bc6307478
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2023-05-25 16:20:03 +0330

    Generalize `main.js` from the previous repo

--- src/main.js
+++ src/main.js
@@ -1,57 +1,45 @@
 #!/usr/bin/env node
 
 /**
- * Created on 1400/4/1 (2021/6/22).
+ * Created on 1402/3/4 (2023/5/25).
  * @author {@link https://mirismaili.github.io S. Mahdi Mir-Ismaili}
  */
 
-import {ESLint} from 'eslint'
 import {exec as legacyExec} from 'node:child_process'
 import {readFile} from 'node:fs/promises'
+import {resolve} from 'node:path'
+import {pathToFileURL} from 'node:url'
 import util from 'node:util'
 
 const exec = util.promisify(legacyExec)
+
+const {preCommitScript} = JSON.parse(await readFile('package.json', 'utf-8'))
+
+if (!preCommitScript) {
+  console.info('No script specified to be done before commit. To specify a one, put its path in `preCommitScript` field in your "package.json" file. Only Node.js scripts are acceptable.')
+  process.exit(0)
+}
 
 const {stdout, stderr} = await exec(
   'git diff --cached --name-only --diff-filter=ACMRTUXB', // `ACMRTUXB` => all changes except "Delete". See:
   // https://git-scm.com/docs/git-diff#Documentation/git-diff.txt---diff-filterACDMRTUXB82308203
 )
+if (stderr) console.error(stderr)
 
-if (stderr) throw Error(stderr)
-
-const eslint = new ESLint()
-
-const files = (await Promise.all(
-  stdout.split('\n')
-    .filter((relativePath) => /\.m?[jt]sx?$/.test(relativePath))
-    .map(
-      async (relativePath) => await eslint.isPathIgnored(relativePath).then((ignored) => !ignored && relativePath),
-    ),
-)).filter(Boolean)
+const files = stdout.trimEnd().split('\n')
 
 if (!files.length) {
-  console.info('No file to lint before commit.')
-  process.exit()
+  console.info('No new/modified file to commit.')
+  process.exit(0)
 }
-console.info('>> Linting:', '\n\t' + files.join('\n\t'), '\n')
-const results = await eslint.lintFiles(files)
-
-console.info((await eslint.loadFormatter('stylish')).format(results))
-
-const [numErrors, numWarnings] = results.reduce(
-  ([numErrors, numWarnings], {errorCount, warningCount}) => [numErrors + errorCount, numWarnings + warningCount],
-  [0, 0],
+console.info(
+  'Processing added/modified files using %s:\n\t%s\n',
+  resolve(preCommitScript),
+  files.map((file) => resolve(file)).join('\n\t'),
 )
+const {default: preCommit} = await import((pathToFileURL(preCommitScript).href))
 
-// noinspection JSUnresolvedVariable
-/** @type {{breakOnWarningsToo: boolean}} */
-const config = JSON.parse(await readFile('package.json', 'utf-8')).eslintPreCommit ?? {}
-
-const numUnacceptableProblems = numErrors + (config.breakOnWarningsToo ? numWarnings : 0)
-
-console.info('\n' + (
-  numUnacceptableProblems
-    ? `Pre-commit process FAILED because of ${numUnacceptableProblems} unacceptable ${numUnacceptableProblems === 1 ? 'problem' : 'problems'}!`
-    : 'Pre-commit process SUCCEEDED.'
-))
-process.exit(numUnacceptableProblems)
+await preCommit(files).catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
